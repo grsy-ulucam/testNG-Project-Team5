bd94cc501312f654f299ee3fdec3b1742878be1e
Merve C <114129176+MrvTC@users.noreply.github.com>
2023-01-15 17:14:35 +0300

    Revert "firs commit"

---
+++ src/test/java/Deneme02.java
@@ -0,0 +1,6 @@
+public class Deneme02 {
+    public static void main(String[] args) {
+
+        System.out.println("merve");
+    }
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+public class TestClass {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("ilk Merve ");
+
+
+
+        System.out.println("sumeyra");
+
+    }
+}
